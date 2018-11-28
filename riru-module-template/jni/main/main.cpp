1a14b3b855f06b9bc507bd2c41d52f699d931faf
RikkaW <rikka@shizuku.moe>
2018-11-28 15:24:38 +0800

    add comment to template module

--- riru-module-template/jni/main/main.cpp
+++ riru-module-template/jni/main/main.cpp
@@ -14,6 +14,12 @@ void nativeForkAndSpecializePre(JNIEnv *env, jclass clazz, jint uid, jint gid, j
 
 __attribute__((visibility("default")))
 int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
+    if (res ==  0) {
+        // in app process
+    } else {
+        // in zygote process, res is child pid
+        // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
+    }
     return 0;
 }
 
@@ -25,6 +31,12 @@ void nativeForkSystemServerPre(JNIEnv *env, jclass clazz, uid_t uid, gid_t gid, 
 
 __attribute__((visibility("default")))
 int nativeForkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
+    if (res ==  0) {
+        // in system server process
+    } else {
+        // in zygote process, res is child pid
+        // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
+    }
     return 0;
 }
 }
