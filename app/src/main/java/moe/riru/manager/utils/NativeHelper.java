21f73734fc2fabde4615c0b723a0f013dbc23c7c
RikkaW <rikka@shizuku.moe>
2019-04-11 19:41:11 +0800

    update app

--- app/src/main/java/moe/riru/manager/utils/NativeHelper.java
+++ app/src/main/java/moe/riru/manager/utils/NativeHelper.java
@@ -1,12 +1,38 @@
 package moe.riru.manager.utils;
 
+import android.content.Context;
+
 import androidx.annotation.Keep;
+import moe.riru.manager.R;
 
 @Keep
 public class NativeHelper {
 
+    private static final int MAX_VERSION = 19;
+    private static final int V18 = 19;
+    private static final int V17_1 = 18;
+
     static {
         System.loadLibrary("helper");
+    }
+
+    public static String versionName(Context context, int versionCode) {
+        if (versionCode > MAX_VERSION) {
+            return context.getString(R.string.riru_unknown_version, versionCode);
+        }
+
+        if (versionCode <= 0) {
+            return context.getString(R.string.riru_not_found);
+        }
+
+        switch (versionCode) {
+            case V18:
+                return "v18";
+            case V17_1:
+                return "v17.1";
+            default:
+                return "v" + versionCode;
+        }
     }
 
     public static native boolean init();
