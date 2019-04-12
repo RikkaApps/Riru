9ce1df9d59a55e15b08eeabb86c43d4d7c5d1ff2
RikkaW <rikka@shizuku.moe>
2019-04-13 01:52:50 +0800

    auto restart zygote is optionable

--- riru-core/template_override/post-fs-data.sh
+++ riru-core/template_override/post-fs-data.sh
@@ -6,4 +6,4 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 ZYGOTE_RESTART=/data/misc/riru/bin/zygote_restart
-$ZYGOTE_RESTART
+[[ ! -f "/data/misc/riru/config/disable_auto_restart" ]] && $ZYGOTE_RESTART
