ca63f752cd1d0337709f7cfab3db53dc182d6d24
RikkaW <rikka@shizuku.moe>
2019-04-26 22:35:38 +0800

    add missing dependency

--- app/src/main/java/moe/riru/manager/utils/NativeHelper.java
+++ app/src/main/java/moe/riru/manager/utils/NativeHelper.java
@@ -3,6 +3,7 @@ package moe.riru.manager.utils;
 import android.content.Context;
 
 import androidx.annotation.Keep;
+
 import moe.riru.manager.R;
 
 @Keep
@@ -36,12 +37,20 @@ public class NativeHelper {
     }
 
     public static native boolean init();
+
     public static native boolean isRiruModuleExists(String name);
+
     public static native int getRiruVersion();
+
     public static native boolean isZygoteMethodsReplaced();
+
     public static native int getNativeForkAndSpecializeCallsCount();
+
     public static native int getNativeForkSystemServerCallsCount();
+
     public static native String getNativeForkAndSpecializeSignature();
+
     public static native String getNativeSpecializeBlastulaSignature();
+
     public static native String getNativeForkSystemServerSignature();
 }
