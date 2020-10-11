92a48e2f0f9cebc373525a58d76c941e8a759a52
RikkaW <rikka@xing.moe>
2020-10-11 21:27:46 +0800

    move old riru.c to header

--- module/src/main/cpp/include/riru.h
+++ module/src/main/cpp/include/riru.h
@@ -107,11 +107,39 @@ void* init(void *arg) RIRU_EXPORT;
 extern int riru_api_version;
 extern RiruApiV9 *riru_api_v9;
 
-void *riru_get_func(const char *name);
-void *riru_get_native_method_func(const char *className, const char *name, const char *signature);
-const JNINativeMethod *riru_get_original_native_methods(const char *className, const char *name, const char *signature);
-void riru_set_func(const char *name, void *func);
-void riru_set_native_method_func(const char *className, const char *name, const char *signature, void *func);
+inline void *riru_get_func(const char *name) {
+    if (riru_api_version == 9) {
+        return riru_api_v9->getFunc(riru_api_v9->token, name);
+    }
+    return NULL;
+}
+
+inline void *riru_get_native_method_func(const char *className, const char *name, const char *signature) {
+    if (riru_api_version == 9) {
+        return riru_api_v9->getJNINativeMethodFunc(riru_api_v9->token, className, name, signature);
+    }
+    return NULL;
+}
+
+inline const JNINativeMethod *riru_get_original_native_methods(const char *className, const char *name, const char *signature) {
+    if (riru_api_version == 9) {
+        return riru_api_v9->getOriginalJNINativeMethodFunc(className, name, signature);
+    }
+    return NULL;
+}
+
+inline void riru_set_func(const char *name, void *func) {
+    if (riru_api_version == 9) {
+        riru_api_v9->setFunc(riru_api_v9->token, name, func);
+    }
+}
+
+inline void riru_set_native_method_func(const char *className, const char *name, const char *signature,
+                                 void *func) {
+    if (riru_api_version == 9) {
+        riru_api_v9->setJNINativeMethodFunc(riru_api_v9->token, className, name, signature, func);
+    }
+}
 #endif
 
 #ifdef __cplusplus
