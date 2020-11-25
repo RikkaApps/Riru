19aa0daeddd879b48d88711083d14d0f6174df91
RikkaW <rikka@xing.moe>
2020-11-25 23:53:08 +0800

    Fix setsockcreatecon

--- riru/src/main/cpp/util/selinux.cpp
+++ riru/src/main/cpp/util/selinux.cpp
@@ -1,5 +1,26 @@
 #include <unistd.h>
 #include <dlfcn.h>
+#include <fcntl.h>
+#include <cstring>
+#include <cerrno>
+
+static int setsockcreatecon_builtin_impl(const char *ctx) {
+    int fd = open("/proc/thread-self/attr/sockcreate", O_RDWR | O_CLOEXEC);
+    if (fd < 0)
+        return fd;
+    ssize_t rc;
+    if (ctx) {
+        do {
+            rc = write(fd, ctx, strlen(ctx) + 1);
+        } while (rc < 0 && errno == EINTR);
+    } else {
+        do {
+            rc = write(fd, nullptr, 0);
+        } while (rc < 0 && errno == EINTR);
+    }
+    close(fd);
+    return rc == -1 ? -1 : 0;
+}
 
 static int stub(const char *) {
     return 0;
@@ -7,19 +28,14 @@ static int stub(const char *) {
 
 int (*setsockcreatecon)(const char *con) = stub;
 
+void selinux_builtin_impl() {
+    setsockcreatecon = setsockcreatecon_builtin_impl;
+}
+
 void dload_selinux() {
     if (access("/system/lib/libselinux.so", F_OK) != 0) {
         return;
     }
 
-
-#ifdef __LP64__
-    void *handle = dlopen("/system/lib64/libselinux.so", RTLD_LAZY | RTLD_LOCAL);
-#else
-    void *handle = dlopen("/system/lib/libselinux.so", RTLD_LAZY | RTLD_LOCAL);
-#endif
-
-    if (!handle) return;
-
-    setsockcreatecon = (int (*)(const char *)) dlsym(handle, "setsockcreatecon");
+    selinux_builtin_impl();
 }
