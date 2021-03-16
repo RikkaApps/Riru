5d635e8c66a018b5086f28e083b18b0d0656022b
LoveSy <shana@zju.edu.cn>
2021-03-16 21:46:16 +0800

    Remove riru & riru modules' soinfos from solist

--- riru/src/main/cpp/hide_utils.cpp
+++ riru/src/main/cpp/hide_utils.cpp
@@ -4,178 +4,204 @@
 #include <link.h>
 #include <string>
 #include <magisk.h>
-//#include <dobby.h>
+#include <dobby.h>
 #include <android_prop.h>
 #include "hide_utils.h"
 #include "wrap.h"
 #include "logging.h"
 #include "module.h"
-//#include "bionic_linker_restriction.h"
+#include <bionic_linker_restriction.h>
 #include "entry.h"
+#include <iostream>
 
 namespace Hide {
+    class ProtectedDataGuard {
 
-    static const char *GetLinkerPath() {
-#if __LP64__
-        if (AndroidProp::GetApiLevel() >= 29) {
-            return "/apex/com.android.runtime/bin/linker64";
-        } else {
-            return "/system/bin/linker64";
+    public:
+        ProtectedDataGuard() {
+            if (ctor != nullptr)
+                (this->*ctor)();
         }
-#else
-        if (AndroidProp::GetApiLevel() >= 29) {
-            return "/apex/com.android.runtime/bin/linker";
-        } else {
-            return "/system/bin/linker";
-        }
-#endif
-    }
 
-    struct hide_data {
-        const char **paths;
-        int paths_count;
+        ~ProtectedDataGuard() {
+            if (dtor != nullptr)
+                (this->*dtor)();
+        }
+
+    public:
+        ProtectedDataGuard(const ProtectedDataGuard &) = delete;
+
+        void operator=(const ProtectedDataGuard &) = delete;
+
+    private:
+        using FuncType = void (ProtectedDataGuard::*)();
+
+        static FuncType ctor;
+        static FuncType dtor;
+
+        union MemFunc {
+            FuncType f;
+
+            struct {
+                void *p;
+                std::ptrdiff_t adj;
+            } data;
+        };
     };
 
-    static int callback(struct dl_phdr_info *info, size_t size, void *_data) {
-        auto data = (hide_data *) _data;
+    ProtectedDataGuard::FuncType ProtectedDataGuard::ctor = MemFunc{.data = {.p = DobbySymbolResolver(
+            nullptr, "__dl__ZN18ProtectedDataGuardC2Ev"),
+            .adj = 0}}
+            .f;
+    ProtectedDataGuard::FuncType ProtectedDataGuard::dtor = MemFunc{.data = {.p = DobbySymbolResolver(
+            nullptr, "__dl__ZN18ProtectedDataGuardD2Ev"),
+            .adj = 0}}
+            .f;
 
-        for (int i = 0; i < data->paths_count; i++) {
-            if (strcmp(data->paths[i], info->dlpi_name) == 0) {
-                memset((void *) info->dlpi_name, 0, strlen(data->paths[i]));
-                LOGD("hide %s from dl_iterate_phdr", data->paths[i]);
-                return 0;
+    namespace {
+        const char *GetLinkerPath() {
+#if __LP64__
+            if (AndroidProp::GetApiLevel() >= 29) {
+                return "/apex/com.android.runtime/bin/linker64";
+            } else {
+                return "/system/bin/linker64";
             }
+#else
+            if (AndroidProp::GetApiLevel() >= 29) {
+                return "/apex/com.android.runtime/bin/linker";
+            } else {
+                return "/system/bin/linker";
+            }
+#endif
         }
-        return 0;
-    }
 
-
-    static void HidePathsFromSolist(const char **paths, int paths_count) {
-        auto data = hide_data{
-                .paths = paths,
-                .paths_count = paths_count
-        };
-        dl_iterate_phdr(callback, &data);
-
-        /*using solist_remove_soinfo_t = bool(soinfo_t soinfo);
+        using solist_remove_soinfo_t = bool(soinfo_t soinfo);
 
         auto solist_remove_soinfo = (solist_remove_soinfo_t *)
                 DobbySymbolResolver(GetLinkerPath(), "__dl__Z20solist_remove_soinfoP6soinfo");
 
-        if (solist_remove_soinfo != nullptr) {
-            static const char **_paths;
-            static int _paths_count;
-            static std::vector<soinfo_t> soinfo_to_remove;
-
-            _paths = paths;
-            _paths_count = paths_count;
-
-            static uintptr_t pagesize = sysconf(_SC_PAGE_SIZE);
-
+        [[maybe_unused]] auto dummy = []() {
             linker_iterate_soinfo([](soinfo_t soinfo) {
-                auto start = (((uintptr_t) soinfo - 1024 + pagesize - 1) & ~(pagesize - 1));
-                if (mprotect((void *) start, 1024, PROT_READ | PROT_WRITE) != 0) {
-                    PLOGE("mprotect %p (%p)", soinfo, (void *) start);
-                } else {
-                    LOGD("mprotect %p (%p)", soinfo, (void *) start);
-                }
-
-                const char *real_path = linker_soinfo_get_realpath(soinfo);
-                if (real_path != nullptr) {
-                    for (int i = 0; i < _paths_count; i++) {
-                        if (strcmp(_paths[i], real_path) == 0) {
-                            LOGD("remove soinfo %s", real_path);
-                            soinfo_to_remove.emplace_back(soinfo);
-                            break;
-                        }
-                    }
-                }
+                [[maybe_unused]] const char *real_path = linker_soinfo_get_realpath(soinfo);
                 return 0;
             });
+            return nullptr;
+        }();
 
-            for (auto soinfo : soinfo_to_remove) {
-                solist_remove_soinfo(soinfo);
-                LOGD("remove soinfo %p", soinfo);
+        void HidePathsFromSolist(const std::vector<const char *> &names) {
+            if (solist_remove_soinfo != nullptr) {
+                static std::vector<const char *> const *_names;
+                static std::vector<soinfo_t> soinfo_to_remove;
+
+                _names = &names;
+
+                static uintptr_t pagesize = sysconf(_SC_PAGE_SIZE);
+
+                linker_iterate_soinfo([](soinfo_t soinfo) {
+                    const char *real_path = linker_soinfo_get_realpath(soinfo);
+                    if (real_path != nullptr) {
+                        for (const auto &_path : *_names) {
+                            if (strcmp(_path, real_path) == 0) {
+                                LOGD("remove soinfo %s", real_path);
+                                soinfo_to_remove.emplace_back(soinfo);
+                                break;
+                            }
+                        }
+                    }
+                    return 0;
+                });
+
+                ProtectedDataGuard g;
+
+                for (auto soinfo : soinfo_to_remove) {
+                    solist_remove_soinfo(soinfo);
+                    LOGD("remove soinfo %p", soinfo);
+                }
+                LOGD("after removed");
+                linker_iterate_soinfo([](soinfo_t soinfo) {
+                    const char *real_path = linker_soinfo_get_realpath(soinfo);
+                    LOGD("rested soinfo %s", real_path);
+                    return 0;
+                });
+            } else {
+                LOGE("cannot find solist_remove_soinfo");
             }
-        } else {
-            LOGE("cannot find solist_remove_soinfo");
-        }*/
-    }
-
-    static void HidePathsFromMaps(const char **paths, int paths_count) {
-        auto hide_lib_path = Magisk::GetPathForSelfLib("libriruhide.so");
-
-        // load riruhide.so and run the hide
-        LOGD("dlopen libriruhide");
-        auto handle = dlopen_ext(hide_lib_path.c_str(), 0);
-        if (!handle) {
-            LOGE("dlopen %s failed: %s", hide_lib_path.c_str(), dlerror());
-            return;
-        }
-        using riru_hide_t = int(const char **names, int names_count);
-        auto *riru_hide = (riru_hide_t *) dlsym(handle, "riru_hide");
-        if (!riru_hide) {
-            LOGE("dlsym failed: %s", dlerror());
-            return;
         }
 
-        LOGD("do hide");
-        riru_hide(paths, paths_count);
+        void HidePathsFromMaps(const std::vector<const char *> &names) {
+            auto hide_lib_path = Magisk::GetPathForSelfLib("libriruhide.so");
 
-        // cleanup riruhide.so
-        LOGD("dlclose");
-        if (dlclose(handle) != 0) {
-            LOGE("dlclose failed: %s", dlerror());
-            return;
+            // load riruhide.so and run the hide
+            LOGD("dlopen libriruhide");
+            auto handle = dlopen_ext(hide_lib_path.c_str(), 0);
+            if (!handle) {
+                LOGE("dlopen %s failed: %s", hide_lib_path.c_str(), dlerror());
+                return;
+            }
+            using riru_hide_t = int(const char *const *names, int names_count);
+            auto *riru_hide = (riru_hide_t *) dlsym(handle, "riru_hide");
+            if (!riru_hide) {
+                LOGE("dlsym failed: %s", dlerror());
+                return;
+            }
+
+            LOGD("do hide");
+            riru_hide(&names[0], names.size());
+
+            // cleanup riruhide.so
+            LOGD("dlclose");
+            if (dlclose(handle) != 0) {
+                LOGE("dlclose failed: %s", dlerror());
+                return;
+            }
+
+            procmaps_iterator *maps = pmparser_parse(-1);
+            if (maps == nullptr) {
+                LOGE("cannot parse the memory map");
+                return;
+            }
+
+            procmaps_struct *maps_tmp;
+            while ((maps_tmp = pmparser_next(maps)) != nullptr) {
+                if (!strstr(maps_tmp->pathname, "/libriruhide.so")) continue;
+
+                auto start = (uintptr_t) maps_tmp->addr_start;
+                auto end = (uintptr_t) maps_tmp->addr_end;
+                auto size = end - start;
+                LOGV("%" PRIxPTR"-%" PRIxPTR" %s %ld %s", start, end, maps_tmp->perm,
+                     maps_tmp->offset,
+                     maps_tmp->pathname);
+                munmap((void *) start, size);
+            }
+            pmparser_free(maps);
         }
-
-        procmaps_iterator *maps = pmparser_parse(-1);
-        if (maps == nullptr) {
-            LOGE("cannot parse the memory map");
-            return;
-        }
-
-        procmaps_struct *maps_tmp;
-        while ((maps_tmp = pmparser_next(maps)) != nullptr) {
-            if (!strstr(maps_tmp->pathname, "/libriruhide.so")) continue;
-
-            auto start = (uintptr_t) maps_tmp->addr_start;
-            auto end = (uintptr_t) maps_tmp->addr_end;
-            auto size = end - start;
-            LOGV("%" PRIxPTR"-%" PRIxPTR" %s %ld %s", start, end, maps_tmp->perm, maps_tmp->offset, maps_tmp->pathname);
-            munmap((void *) start, size);
-        }
-        pmparser_free(maps);
     }
 
     void DoHide(bool solist, bool maps) {
         auto self_path = Magisk::GetPathForSelfLib("libriru.so");
         auto modules = Modules::Get();
-        auto names = (const char **) malloc(sizeof(char *) * modules.size());
-        int names_count = 0;
+        std::vector<const char *> names{};
         for (auto module : Modules::Get()) {
             if (strcmp(module->id, MODULE_NAME_CORE) == 0) {
                 if (Entry::IsSelfUnloadAllowed()) {
                     LOGD("don't hide self since it will be unloaded");
-                    continue;
                 }
-                names[names_count] = self_path.c_str();
+                names.push_back(self_path.c_str());
+            } else if (module->apiVersion <= 24) {
+                LOGD("%s is too old to hide", module->id);
             } else if (module->supportHide) {
                 if (!module->isLoaded()) {
                     LOGD("%s is unloaded", module->id);
-                    continue;
+                } else {
+                    names.push_back(module->path);
                 }
-                names[names_count] = module->path;
             } else {
                 LOGD("module %s does not support hide", module->id);
-                continue;
             }
-            names_count += 1;
         }
-        if (names_count > 0) {
-            if (solist) Hide::HidePathsFromSolist(names, names_count);
-            if (maps) Hide::HidePathsFromMaps(names, names_count);
+        if (!names.empty()) {
+            if (solist) Hide::HidePathsFromSolist(names);
+            if (maps) Hide::HidePathsFromMaps(names);
         }
-        free(names);
     }
 }
