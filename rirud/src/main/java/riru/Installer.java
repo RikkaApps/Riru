3493d975ce428df6a85bbbf81c9b7f937c4c15ee
RikkaW <rikka@shizuku.moe>
2021-07-19 09:52:55 +0800

    Print information when checking SELinux rules

--- rirud/src/main/java/riru/Installer.java
+++ rirud/src/main/java/riru/Installer.java
@@ -5,16 +5,43 @@ import android.os.SELinux;
 public class Installer {
 
     private static void checkSELinux() {
-        if (DaemonUtils.hasSELinux() && SELinux.isSELinuxEnabled() && SELinux.isSELinuxEnforced()
-                && (SELinux.checkSELinuxAccess("u:r:init:s0", "u:object_r:system_file:s0", "file", "relabelfrom")
-                || SELinux.checkSELinuxAccess("u:r:init:s0", "u:object_r:system_file:s0", "dir", "relabelfrom"))) {
+        if (!DaemonUtils.hasSELinux()) {
+            System.out.println("- This device does not have SELinux");
+            return;
+        }
+        if (!SELinux.isSELinuxEnabled()) {
+            System.out.println("- SELinux is disabled");
+            return;
+        }
+        if (!SELinux.isSELinuxEnforced()) {
+            System.out.println("! SELinux is permissive");
+            System.out.println("! If your ROM runs Android 10+ and has SELinux permissive (it will disable seccomp), ANY app can escalate itself to uid 0, this is extremely dangerous");
+            System.out.println("- Just a kind reminder, continue installation...");
+            return;
+        }
+
+        boolean exit = false;
+        if (SELinux.checkSELinuxAccess("u:r:init:s0", "u:object_r:system_file:s0", "file", "relabelfrom")) {
+            System.out.println("! Your ROM allows init to relabel Magisk module files");
+            exit = true;
+        }
+
+        if (SELinux.checkSELinuxAccess("u:r:init:s0", "u:object_r:system_file:s0", "dir", "relabelfrom")) {
+            System.out.println("! Your ROM allows init to relabel Magisk module files");
+            exit = true;
+        }
+
+        if (exit) {
             System.exit(1);
+        } else {
+            System.out.println("- No problem found");
         }
     }
 
     public static void main(String[] args) {
         for (String arg : args) {
             if ("--check-selinux".equals(arg)) {
+                System.out.println("- Start checks...");
                 checkSELinux();
                 return;
             }
