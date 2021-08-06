66d4c0851abd1034f5977aaa27fb2c36d48e2bbb
LoveSy <shana@zju.edu.cn>
2021-08-06 20:49:31 +0800

    Fix service.sh flock

--- template/magisk_module/service.sh
+++ template/magisk_module/service.sh
@@ -1,3 +1,6 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+if [ -z "$SHELL" ]; then
+  export SHELL=sh
+fi
 flock -n "$MODDIR/module.prop" -c "sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ post-fs-data.sh fails to run. Magisk is broken on this device. ] /g' \"$MODDIR/module.prop\""
