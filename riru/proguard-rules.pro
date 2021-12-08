6d63386347ecf0ec8b35ba6c2e105a1e4a3fdad7
vvb2060 <vvb2060@gmail.com>
2021-12-08 16:49:44 +0800

    Update gradle

--- riru/proguard-rules.pro
+++
@@ -1,6 +0,0 @@
--keep class riru.Daemon {
-    public static void main(java.lang.String[]);
-}
--keep class riru.Installer {
-    public static void main(java.lang.String[]);
-}
--- stub/build.gradle
+++ stub/build.gradle
@@ -3,6 +3,6 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
